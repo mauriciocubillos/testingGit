18a9709705fc6283be2f41b066e0783b29be8c7a
Mao Cubillos <mauricio.cubillos@globant.com>
2013-06-20 11:59:41 -0500

    Adding lines to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 var appName = process.argv[2], //0 = node, 1 = js file, 2 = app name, 3 = options
     appOptions = process.argv[3],
     app = require('./app-express.js')(appName, appOptions);
+var appName = process.argv[2], //0 = node, 1 = js file, 2 = app name, 3 = options
+    appOptions = process.argv[3],
+    app = require('./app-express.js')(appName, appOptions);
